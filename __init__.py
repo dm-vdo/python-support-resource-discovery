9f16f2cc82b621e52dee6a1e3a45a327e814d254
Joe Shimkus <jshimkus@redhat.com>
2019-10-18 15:26:59 -0400

    Export the specific architectures for reference.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 from .architecture import Architecture, ArchitectureFactory
+from .architecture import AArch64, PPC64, PPC64LE, S390X, X86_64
--- architecture/__init__.py
+++ architecture/__init__.py
@@ -1,2 +1,3 @@
 from .Architecture import Architecture
 from .ArchitectureFactory import ArchitectureFactory
+from .architectures import AArch64, PPC64, PPC64LE, S390X, X86_64
