a61fc9f7b3abb158bf18bc95191487e3171c44fc
Joe Shimkus <jshimkus@redhat.com>
2021-01-12 14:51:50 -0500

    Add display of default choice name.

--- test.py
+++ test.py
@@ -9,3 +9,4 @@ from architecture import Architecture, FactoryShell
 if __name__ == "__main__":
   shell = FactoryShell(Architecture)
   shell.printChoices()
+  print("Default choice: {0}".format(Architecture.defaultChoice().name()))
