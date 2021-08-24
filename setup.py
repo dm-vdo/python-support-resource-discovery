ad44a23d66eb1dab05d6fab9905d248031ae98b8
Joe Shimkus <jshimkus@redhat.com>
2021-08-24 13:06:29 -0400

    Changes for rpm generation for py2 and py3.

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
+import platform
 import setuptools
 
+python_version = platform.python_version_tuple()[0]
+
 setuptools.setup(
-  name = "repos",
+  name = "python{0}-infrastructure-repos".format(python_version),
   version = "1.0.0",
-  description = "repos",
+  description = "python{0}-infrastructure-repos".format(python_version),
   author = "Joe Shimkus",
   author_email = "jshimkus@redhat.com",
-  packages = setuptools.find_packages(exclude = []),
-  install_requires = ["architectures", "command"],
-  dependency_links = [
-                      "http://vdo-image-store.permabit.lab.eng.bos.redhat.com/repository/jshimkus/"
-                     ]
+  package_dir = {"" : "repos"},
+  packages = setuptools.find_packages(where = "repos",
+                                      exclude = []),
+  # setuptools doesn't actually put the content of "requires" in the generated
+  # rpm spec file though it claims that it should.
+  # We'll just make certain to always install the requirements manually, for
+  # which this serves as a reminder of what to install.
+  requires = ["architectures", "command"]
 )
