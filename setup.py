b9b33f35f11aa5898b3a4da2d1abf32ac2e8f918
Joe Shimkus <jshimkus@redhat.com>
2021-09-30 13:55:40 -0400

    Save available roots locally.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def prefixed(src):
 
 setuptools.setup(
   name = prefixed("repos"),
-  version = "1.0.1",
+  version = "1.0.2",
   description = prefixed("repos"),
   author = "Joe Shimkus",
   author_email = "jshimkus@redhat.com",
