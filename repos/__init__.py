cb2142469fc20bb0e2adb75e6fcfa2657471a883
Joe Shimkus <jshimkus@redhat.com>
2021-12-02 18:08:52 -0500

    Install repos command.
    Provides what repositories are available.

--- repos/__init__.py
+++ repos/__init__.py
@@ -2,3 +2,7 @@ from .CentOS import CentOS
 from .Fedora import Fedora
 from .ReposCommand import ReposCommand
 from .RHEL import RHEL
+
+import command
+def repos():
+  command.CommandShell(ReposCommand).run()
--- setup.py
+++ setup.py
@@ -5,17 +5,29 @@ import setuptools
 import sys
 
 def prefixed(src):
+  if ("bdist_wheel" not in sys.argv) or ("--universal" not in sys.argv):
+    src = python_prefixed(src)
+  return src
+
+def python_prefixed(src):
+  return "{0}-{1}".format(versioned("python"), src)
+
+def versioned(src):
   python_version = platform.python_version_tuple()[0]
   if ("bdist_wheel" in sys.argv) and ("--universal" in sys.argv):
     python_version = ""
-  return "python{0}-{1}".format(python_version, src)
+  return "{0}{1}".format(src, python_version)
 
 setuptools.setup(
   name = prefixed("repos"),
-  version = "1.0.7",
+  version = "1.1.0",
   description = prefixed("repos"),
   author = "Joe Shimkus",
   author_email = "jshimkus@redhat.com",
   packages = setuptools.find_packages(exclude = []),
+  entry_points = {
+    "console_scripts" :
+      "{0} = repos:repos".format(versioned("repos"))
+  },
   install_requires = [prefixed("architectures"), prefixed("command")]
 )
